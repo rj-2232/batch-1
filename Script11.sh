96f99ddc3451593a197ca40d4631c594cedb23c6
rj-2232 <roshanmine111@gmail.com>
2025-07-29 17:42:58 +0530

    Update Script11.sh

--- Script11.sh
+++ Script11.sh
@@ -17,24 +17,4 @@ else
  echo "None of the condition met"
 fi
 
-echo "I have changed this branch"
-
-fi
-
-echo "I have changed this branch"
-echo "I have changed this branch"
-
-fi
-
-echo "I have changed this branch"
-echo "I have changed this branch"
-
-fi
-
-echo "I have changed this branch"
-echo "I have changed this branch"
-
-fi
-
-echo "I have changed this branch"
 
